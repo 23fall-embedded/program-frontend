669b6a065e7daccf38e3081fe9e65d2e3dc35761
Dusker233 <1187305740@qq.com>
2023-12-22 15:40:34 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,85 @@
+// index.js
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: '',
+    region: ['山东省', '威海市', '环翠区'],
+    now: {
+      temp: 0,
+      text: '未知',
+      vis: 0
+    }
+  },
+  //触发事件
+  houduan: function () {
+    var that = this;
+    wx.request({
+      url: 'http://localhost:8080/toJson',
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {//成功交互后触发
+        console.log(res.data)//打印到控制台
+        
+      }
+    })
+  } ,
+  regionChange: function (e) {
+    this.setData({region: e.detail.value})
+    this.getLocationID().then(result => {
+      this.getWeatherInfo()
+    })
+  },
+
+  getWeatherInfo: function (){
+    var that = this
+    wx.request({
+      url: 'https://devapi.qweather.com/v7/weather/now',
+      data: {
+        location: that.data.locationID,
+        key: 'b1aa1ec43dd04106bfc66420636d2e5c'
+      },
+      success: function(res) {
+        that.setData({now: res.data.now})
+        //console.log(res.data)
+      }
+    })
+  },
+
+  getLocationID: function() {
+    var that = this
+    return new Promise(  //解决异步问题
+      resolve => {
+        wx.request({
+          url: 'https://geoapi.qweather.com/v2/city/lookup?',
+          data: {
+            location: that.data.region[1],
+            adm: that.data.region[0],  //防止地名重复，引入参数adm
+            key: 'b1aa1ec43dd04106bfc66420636d2e5c'
+          },
+          success: res => {
+            that.setData({locationID: res.data.location[0].id})
+            return resolve()
+          }
+        })
+      }
+    )
+    
+  },
+
+  onLoad() {
+    this.getLocationID().then(result => {
+      this.getWeatherInfo()
+    })
+  }
+  
+  
+})
+
+
