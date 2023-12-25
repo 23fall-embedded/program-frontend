7ff4df03d83d929f935992442c8f095a5a78a82a
Clay-KoSaking <2974107908@qq.com>
2023-12-25 09:44:36 +0800

    except photosensitive sensor

--- 2023fall-12/pages/index/index.js
+++ 2023fall-12/pages/index/index.js
@@ -38,28 +38,80 @@ Page({
         name: 'all'
       }
     ],
-    LEDindex: 0
+    LEDindex: 0,
+    temperature: 0,
+    humidity: 0,
+    mq3: "否",
+    fire: "否",
+    light: "否",
+    licenseCnt: 0,
+    license_img: "1234",
+    licenseArray: []
   },
+
+  onShow: function () {
+    let that = this
+    return new Promise(
+      resolve => {
+        that.houduan()
+      }
+    )
+  },
+
   //触发事件
   houduan: function () {
     var that = this;
-    wx.request({
-      url: 'http://localhost:8080/toJson',
-      method: 'GET',
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {//成功交互后触发
-        console.log(res.data)//打印到控制台
-        
-      }
-    })
-  } ,
+    setInterval(function () {
+      wx.request({
+        url: 'http://localhost:8080/getData',
+        method: 'GET',
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success: function (res) {//成功交互后触发
+          console.log(res.data)//打印到控制台
+          let mq3Status = (res.data.mq3[0] == "0" ? "否" : "是");
+          let lightStatus = (res.data.light[0] == "0" ? "否" : "是");
+          let fireStatus = (res.data.fire[0] == "0" ? "否" : "是");
+          that.setData({
+            temperature: res.data.temperature[0],
+            humidity: res.data.humidity[0],
+            mq3: mq3Status,
+            light: lightStatus,
+            fire: fireStatus,
+            licenseCnt: res.data.licenses.length,
+            license_img: res.data.img[0],
+            licenseArray: res.data.licenses
+          })
+        }
+      })
+    }, 1000)
+    
+  },
   regionChange: function (e) {
+    let that = this;
     this.setData({region: e.detail.value})
     this.getLocationID().then(result => {
       this.getWeatherInfo()
     })
+    return new Promise(
+      resolve => {
+        wx.request({
+          url: 'http://localhost:8080/sendData',
+          data: {
+            "loc": that.data.region[2],
+            "adm": that.data.region[1]
+          },
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          method: 'POST',
+          success() {
+            console.log('Location success');
+          }
+        })
+      }
+    )
   },
 
   getWeatherInfo: function (){
@@ -84,8 +136,8 @@ Page({
         wx.request({
           url: 'https://geoapi.qweather.com/v2/city/lookup?',
           data: {
-            location: that.data.region[1],
-            adm: that.data.region[0],  //防止地名重复，引入参数adm
+            location: that.data.region[2],
+            adm: that.data.region[1],  //防止地名重复，引入参数adm
             key: 'b1aa1ec43dd04106bfc66420636d2e5c'
           },
           success: res => {
@@ -99,23 +151,30 @@ Page({
   },
 
   LEDChange: function(e) {
+    let that = this;
     this.setData({
       LEDindex: e.detail.value
     })
     console.log(e.detail.value);
-    wx.request({
-      url: 'http://localhost:8080/LED',
-      data: {
-        "LEDid": e.detail.value
-      },
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      method: 'POST',
-      success() {
-        console.log('LED success');
+    console.log(that.data.LEDarray[e.detail.value]);
+    return new Promise(
+      resolve => {
+        wx.request({
+          url: 'http://localhost:8080/sendData',
+          data: {
+            "led": that.data.LEDarray[e.detail.value]
+          },
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          method: 'POST',
+          success() {
+            console.log('LED success');
+          }
+        })
       }
-    })
+    )
+    
   },
 
   gotoLicense: function() {
